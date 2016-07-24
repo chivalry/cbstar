84f670e3e68e0bb9af2822fb97045bbe6bc99458
Charles Ross <chivalry@mac.com>
2016-07-24 16:20:53 -0700

    Finish test_page_count_returns_correct_count.

--- test_comicfile.py
+++ test_comicfile.py
@@ -1,16 +1,15 @@
 import unittest
-from unittest import TestCase
-from unittest.mock import Mock
+import unittest.mock
 
 import os
-from zipfile import ZipFile
+import zipfile
 
-from comicfile import ComicFile
+import comicfile
 
-class TestPruneDirs(TestCase):
+class TestPruneDirs(unittest.TestCase):
 
     def setUp(self):
-        self.comic_file = ComicFile()
+        self.comic_file = comicfile.ComicFile()
         self.dirs = ['/path/to/dir/', 'relative/path/to/dir/']
         self.files = ['/path/to/file', 'relative/path/to/file']
 
@@ -33,37 +32,31 @@ class TestPruneDirs(TestCase):
     def assert_empty(self, list:list):
         self.assertEqual([], list)
 
-class TestPageCount(TestCase):
+class TestPageCount(unittest.TestCase):
 
     def setUp(self):
-        self.comic_file = ComicFile()
+        self.comic_file = comicfile.ComicFile()
 
     def test_page_count_raises_error_when_file_not_set(self):
-        with self.assertRaises(ComicFile.FileNoneError):
+        with self.assertRaises(comicfile.ComicFile.FileNoneError):
             self.comic_file.page_count()
 
     def test_page_count_raises_error_when_file_missing(self):
-        with self.assertRaises(ComicFile.FileNotFoundError):
+        with self.assertRaises(comicfile.ComicFile.FileNotFoundError):
             self.comic_file.file = '/file/does/not/exist.zip'
             self.comic_file.page_count()
         
-    @unittest.skip('working on the right procedure for this one')
-    def test_page_count_returns_file_count(self):
-        script_path = os.path.realpath(__file__)
-        parent_dir = os.path.abspath(os.path.join(script_path, os.pardir))
-        targ_dir = os.path.join(parent_dir, 'files')
-        targ_file = os.path.join(targ_dir, 'Archive.zip' )
-        self.comic_file.file = targ_file
-
+    @unittest.mock.patch('comicfile.ZipFile')
+    def test_page_count_returns_correct_count(self, mock_zip_file):
         # Store as tuples to use as dictionary keys.
         members_dict = {('dir/', 'dir/file1', 'dir/file2'):2,
                         ('file1.jpg', 'file2.jpg', 'file3.jpg'):3
         }
+        
+        # Make the file point to something to prevent FileNoneError.
+        self.comic_file.file = __file__
+
         for file_tuple, count in members_dict.items():
-            test_zip_file = Mock(ZipFile)
-            test_zip_file.namelist.return_value = list(file_tuple)
+            mock_zip_file.return_value.__enter__.return_value.namelist.return_value \
+                    = list(file_tuple)
             self.assertEqual(count, self.comic_file.page_count())
-
-
-
-
