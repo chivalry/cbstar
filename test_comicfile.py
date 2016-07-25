d02cc8f593669f1b9ccd2478b02da4c012161d76
Charles Ross <chivalry@mac.com>
2016-07-25 08:47:25 -0700

    Write `test_delete_page`.

--- test_comicfile.py
+++ test_comicfile.py
@@ -1,6 +1,10 @@
 from unittest import TestCase
 from unittest.mock import patch
 from tempfile import TemporaryDirectory
+from zipfile import ZipFile
+from zipfile import ZIP_DEFLATED
+import os
+import random
 
 from comicfile import ComicFile
 
@@ -53,10 +57,42 @@ class TestComicFile(TestCase):
         p of the new file is equal to page p+1 of the original file.
         """
 
-        # Build a file with 10 "pages" to test.
-        # First create the pages in a temporary directory.
         with TemporaryDirectory() as tmp_dir:
-            pass
+            zip_path = os.path.join(tmp_dir, 'test_delete_page.zip')
+            with ZipFile(zip_path, mode='w', compression=ZIP_DEFLATED) as zip_file:
+                for i in range(10):
+                    file_name = ('0' + str(i))[:2]
+                    with open(os.path.join(tmp_dir, file_name), 'w') as tmp_file:
+                        tmp_file.write(file_name)
+                        zip_file.write(tmp_file.name)
+
+            self.comic_file.file_path = zip_path
+            orig_count = self.comic_file.page_count()
+            orig_pages = self.comic_file.page_names()
+
+            self.comic_file.delete_page()
+            new_count = self.comic_file.page_count()
+            new_pages = self.comic_file.page_names()
+            self.assertEqual(new_count, orig_count - 1)
+            self.assertNotIn('01', new_pages)
+
+            self.comic_file.delete_page(5)
+            new_count = self.comic_file.page_count()
+            new_pages = self.comic_file.page_names()
+            self.assertEqual(new_count, orig_count - 2)
+            self.assertNotIn('06', new_pages)
 
     def prevent_file_none_error(self):
         self.comic_file.file_path = __file__
+
+    def print_info(self, archive_name):
+        zf = ZipFile(archive_name)
+        for info in zf.infolist():
+            print(info.filename)
+            print('\tComment:\t', info.comment)
+            print('\tSystem:\t\t', info.create_system, '(0 = Windows, 3 = Unix)')
+            print('\tZIP version:\t', info.create_version)
+            print('\tCompressed:\t', info.compress_size, 'bytes')
+            print('\tUncompressed:\t', info.file_size, 'bytes')
+            print()
+
