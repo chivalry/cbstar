8f861e6e22a1809143d4319f97ca2f3b57221e02
Charles Ross <chivalry@mac.com>
2016-07-26 11:24:13 -0700

    Add __del__ and get_rar_obj.

--- rar_archiver.py
+++ rar_archiver.py
@@ -1,5 +1,7 @@
 import os
 import platform
+from rarfile import RarFile
+from time import sleep
 
 class RarArchiver:
     """
@@ -22,3 +24,18 @@ class RarArchiver:
             self.startupinfo.dwFlags |= _subprocess.STARTF_USESHOWWINDOW
         else:
             self.startupinfo = None
+
+    def __del__(self):
+        RarArchiver.devnull.close()
+
+    def get_rar_obj(self):
+        for tries in range(7):
+            try:
+                rarc = RarFile(self.path)
+            except (OSError, IOError) as e:
+                print('get_rar_obj(): [{}] {} attempt #{}'.format(e, self.path, tries),
+                      file=os.stderr)
+                time.sleep(1)
+            else:
+                return rar_obj
+        return e
