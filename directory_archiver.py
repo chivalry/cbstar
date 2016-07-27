965e55a1ad6c5f3bfc4bf46eefceaa3c874dbc8d
Charles Ross <chivalry@mac.com>
2016-07-26 17:20:43 -0700

    Add first few functions.

--- directory_archiver.py
+++ directory_archiver.py
@@ -6,4 +6,22 @@ class DirectoryArchiver:
     """
 
     def __init__(self, path):
+        self.path = path
+        self.comment_file_name = 'ComicTaggerFoldercomment.txt'
 
+    def get_comment(self):
+        return self.read_member(self.comment_file_name)
+
+    def set_comment(self, comment):
+        return self.write_member(self.comment_file_name, comment)
+
+    def read_member(self, member):
+        data = ''
+        filename = os.path.join(self.path, member)
+        try:
+            with open(filename, 'rb') as f:
+                data = f.read
+        except IOError as e:
+            pass
+
+        return data
