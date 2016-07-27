44dd361cd782165697002e29ee93e43d176dee41
Charles Ross <chivalry@mac.com>
2016-07-26 17:15:59 -0700

    Begin directory_archiver.py.

---
+++ directory_archiver.py
@@ -0,0 +1,9 @@
+class DirectoryArchiver:
+    """
+    Directory implementation
+    
+    https://github.com/davide-romanini/comictagger/blob/master/comicapi/comicarchive.py
+    """
+
+    def __init__(self, path):
+
