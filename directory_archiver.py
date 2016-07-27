ad79b63a478b67721b5b9f5d25af8e5b1d169121
Charles Ross <chivalry@mac.com>
2016-07-26 17:29:13 -0700

    Finish directory_archiver.

--- directory_archiver.py
+++ directory_archiver.py
@@ -16,7 +16,6 @@ class DirectoryArchiver:
         return self.write_member(self.comment_file_name, comment)
 
     def read_member(self, member):
-        data = ''
         filename = os.path.join(self.path, member)
         try:
             with open(filename, 'rb') as f:
@@ -24,4 +23,34 @@ class DirectoryArchiver:
         except IOError as e:
             pass
 
-        return data
+        return data if data else ''
+
+    def write_member(self, member, data):
+        filename = os.path.join(self.path, member)
+        try:
+            with open(filename, 'w+') as f:
+                f.write(data)
+        except:
+            return False
+        else:
+            return True
+
+    def remove_member(self, member):
+        filename = os.path.join(self.path, member)
+        try:
+            os.remove(filename)
+        except:
+            return False
+        else:
+            return True
+
+    def get_member_filename_list(self):
+        return self.list_files(self.path)
+
+    def list_files(self, folder):
+        members = list()
+
+        for item in os.listdir(folder):
+            members.append(item)
+            if os.path.isdir(member):
+                members.extend(self.list_files(os.path.join(folder, member)))
